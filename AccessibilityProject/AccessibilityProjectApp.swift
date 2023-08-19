bb3c186a63c5ed8b26d75ec5742fe7dcb4d1ff2f
LuiArm <Luisarmendariz24@icloud.com>
2023-08-18 19:28:08 -0600

    Initial Commit

---
+++ AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by luis armendariz on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
